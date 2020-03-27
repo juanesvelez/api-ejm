ae4a076b2b4c2aff888113fc2533484d954c02a6
Juan Esteban Velez M <juanesvelez@gmail.com>
2020-03-27 16:49:57 -0500

    Creacion de routes, services

---
+++ src/startup/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+
+let _express = null;
+let config = null
+
+class Server{
+    constructor({config , router}){
+        _config = config;
+        _express = router
+
+    }
+
+
+    
+}
+
+module.exports = Server;
